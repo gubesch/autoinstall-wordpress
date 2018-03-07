38017b1f3319635e571070e4add54e34e1f8d6ba
Christian Gubesch <christian.gubesch@gmail.com>
2018-03-07 16:14:19 +0100

    Create wordpress.sh

---
+++ wordpress.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#by Chrisitan Gubesch
+#last updated 3/7/2018
+
+function install_wordpress {
+    cd "${1}"
+    wget https://wordpress.org/latest.tar.gz
+    tar xzf latest.tar.gz
+    chown -R www-data:www-data wordpress
+    cp -R wordpress/* .
+    PASSWDDB="$(openssl rand -base64 12)"
+    echo -n "Enter database name: "
+    read MAINDB
+    echo -n "Please enter root user MySQL password:"
+    read -s rootpasswd
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} DEFAULT CHARACTER SET utf8;"
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+
+    rm latest.tar.gz
+    rm wordpress
+
+    ipADDRESS="$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
+
+    echo -e "All your wordpress content is now saved at ${1} !\n"
+    echo "Now go to your browser and open: ${ipADDRESS}"
+    echo "Your database name is: ${MAINDB}"
+    echo "Your database User-Name is: ${MAINDB}"
+    echo "Your password is: ${PASSWDDB}"
+}
+
+
+echo "Welcome to wordpress installation!"
+echo -n "Please enter your wished installation destination: "
+read destination
+echo ""
+
+if [ -d ${destination} ] ; then
+    install_wordpress "${destination}"
+else
+    echo "${destination} doesn't exist"
+    echo -n "do you want to create this folder? (Y/n)"
+    read election
+    if [ "${election}" == "y" ] || [ "${election}" == "Y" ] || [ "${election}" == "" ] ; then
+        mkdir -p "${destination}"
+        install_wordpress "${destination}"
+    else
+        echo -e "\nclosing script"
+    fi
+    
+fi
